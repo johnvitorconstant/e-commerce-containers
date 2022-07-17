6e06648ddd54545c0f5b699fa8ae54c807f61428
John Vitor Constant <johnvitorconstant@gmail.com>
2022-07-17 20:57:30 -0300

    Token handler configured

--- src/services/NSE.Identity.API/Controllers/AuthController.cs
+++ src/services/NSE.Identity.API/Controllers/AuthController.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NSE.Identity.API.Extensions;
 using NSE.Identity.API.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace NSE.Identity.API.Controllers
 {
@@ -10,11 +16,15 @@ namespace NSE.Identity.API.Controllers
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppSettings _appSettings;
 
-        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        public AuthController(SignInManager<IdentityUser> signInManager, 
+            UserManager<IdentityUser> userManager, 
+            IOptions<AppSettings> appSettings)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpPost("signup")]
@@ -45,11 +55,54 @@ namespace NSE.Identity.API.Controllers
         {
             if (!ModelState.IsValid) return BadRequest();
             var result = await _signInManager.PasswordSignInAsync(userSignIn.Email, userSignIn.Password, false, true);
-            
+
             if (result.Succeeded) return Ok();
             return BadRequest();
 
         }
+
+        private async Task<UserResponseLogin> GenerateJwt(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            var claims = await _userManager.GetClaimsAsync(user);
+
+
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.Now).ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.Now).ToString(), ClaimValueTypes.Integer64));
+
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            foreach (var userRole in userRoles)
+            {
+                claims.Add(new Claim("role", userRole));
+            }
+
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+            var token = tokenHandler.CreateToken(
+                new SecurityTokenDescriptor
+                {
+                    Issuer = _appSettings.Issuer,
+                    Audience= _appSettings.ValidAt,
+                    Subject = identityClaims,
+                    Expires = DateTime.UtcNow.AddHours(_appSettings.HoursToExpire),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                });
+
+        }
+
+        private static long ToUnixEpochDate(DateTime date) =>
+            (long)Math
+            .Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
+
 
 
 
---
+++ src/services/NSE.Identity.API/Models/UserResponseLogin.cs
@@ -0,0 +1,22 @@
+﻿namespace NSE.Identity.API.Models
+{
+    public class UserResponseLogin
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UserToken UserToken { get; set; }
+    }
+
+    public class UserToken
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<UserClaim> UserClaims { get; set; }
+    }
+
+    public class UserClaim
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
+    }
+}
