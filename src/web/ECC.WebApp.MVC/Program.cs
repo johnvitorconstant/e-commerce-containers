a0f4a20cc17120b8522b4b38c1acefe5781b9cae
John Vitor Constant <johnvitorconstant@gmail.com>
2022-07-18 19:15:46 -0300

    Merging

--- src/web/ECC.WebApp.MVC/Program.cs
+++ src/web/ECC.WebApp.MVC/Program.cs
@@ -15,7 +15,7 @@ namespace NSE.WebApp.MVC
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The defaulst sHSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
