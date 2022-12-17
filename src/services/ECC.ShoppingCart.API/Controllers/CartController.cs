f18b2183ea2a32a87157166b197146d2bcba6873
John Vitor Constant <johnvitorconstant@gmail.com>
2022-12-17 16:06:37 -0300

    Controllers crated

---
+++ src/services/ECC.ShoppingCart.API/Controllers/CartController.cs
@@ -0,0 +1,37 @@
+﻿using ECC.ShoppingCart.API.Model;
+using ECC.WebAPI.Core.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECC.ShoppingCart.API.Controllers
+{
+    [Authorize]
+    public class CartController : MainController
+    {
+        [HttpGet("cart")]
+        public async Task<CartClient> GetCart()
+        {
+            return null;
+        }
+
+        [HttpPost("cart")]
+        public async Task<IActionResult> AddItemCart(CartItem item)
+        {
+            return CustomResponse();
+        }
+
+        [HttpPut("cart/{productId}")]
+        public async Task<IActionResult> UpdateCartItem(Guid productId, CartItem item)
+        {
+            return CustomResponse();
+        }
+
+        [HttpDelete("cart/{productId}")]
+        public async Task<IActionResult> RemoveCartItem(Guid productId)
+        {
+            return CustomResponse();
+        }
+
+
+    }
+}
